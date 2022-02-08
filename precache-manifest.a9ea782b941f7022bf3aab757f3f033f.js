c12b01757e61c36a122f28d743b09052762e5222
WesleyAldridge <38218248+WesleyAldridge@users.noreply.github.com>
2022-02-08 10:44:31 -0700

    Updates

---
+++ precache-manifest.a9ea782b941f7022bf3aab757f3f033f.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0e7e4c966458a4949b925f35a14f6c14",
+    "url": "/wordle/index.html"
+  },
+  {
+    "revision": "4a09f6fd8a30b0d4c818",
+    "url": "/wordle/static/css/main.acb2ade7.chunk.css"
+  },
+  {
+    "revision": "516cc5363aeaa2a5170f",
+    "url": "/wordle/static/js/2.cf66b61d.chunk.js"
+  },
+  {
+    "revision": "fb6fca4f0fa26a7e27d26480a74532c9",
+    "url": "/wordle/static/js/2.cf66b61d.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "4a09f6fd8a30b0d4c818",
+    "url": "/wordle/static/js/main.a7f2600f.chunk.js"
+  },
+  {
+    "revision": "cdd10b0694e00b928ad5",
+    "url": "/wordle/static/js/runtime-main.4555cfe5.js"
+  }
+]);
