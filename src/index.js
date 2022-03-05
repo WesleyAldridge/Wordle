e3fac03b39d9d0ab998cb73e73ff4278ec63d84e
WesleyAldridge <38218248+WesleyAldridge@users.noreply.github.com>
2022-03-05 09:19:18 -0700

    Added tailwind CSS

--- src/index.js
+++ src/index.js
@@ -43,7 +43,7 @@ class Board extends React.Component {
   constructor(props) {
     super(props);
     this.state = {
-      squares: Array(9).fill(null), 
+      squares: Array(25).fill(null), 
       xIsNext: true,
       gameOver: false,
       winner: null,
@@ -149,18 +149,32 @@ class Board extends React.Component {
           {this.renderSquare(4)}
         </div>
         <div className="board-row">
-          {this.renderSquare(3)}
-          {this.renderSquare(4)}
-          {this.renderSquare(5)
-          {this.renderSquare(6)}
-          {this.renderSquare(7)}}
-        </div>
-        <div className="board-row">
+          {this.renderSquare(5)}
           {this.renderSquare(6)}
           {this.renderSquare(7)}
           {this.renderSquare(8)}
           {this.renderSquare(9)}
+        </div>
+        <div className="board-row">
           {this.renderSquare(10)}
+          {this.renderSquare(11)}
+          {this.renderSquare(12)}
+          {this.renderSquare(13)}
+          {this.renderSquare(14)}
+        </div>
+        <div className="board-row">
+          {this.renderSquare(15)}
+          {this.renderSquare(16)}
+          {this.renderSquare(17)}
+          {this.renderSquare(18)}
+          {this.renderSquare(19)}
+        </div>
+        <div className="board-row">
+          {this.renderSquare(20)}
+          {this.renderSquare(21)}
+          {this.renderSquare(22)}
+          {this.renderSquare(23)}
+          {this.renderSquare(24)}
         </div>
         {this.renderResetButton()}
       </div>
@@ -171,7 +185,8 @@ class Board extends React.Component {
 class Game extends React.Component {
   render() {
     return (
-      <div className="game">
+      <div className="game mx-auto w-96">
+        <h1 className="text-4xl text-center">Wordle</h1>
         <div className="game-board">
           <Board />
         </div>
